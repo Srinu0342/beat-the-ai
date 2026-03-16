28fc29392bdc0a583c4feeb1aea73dda4814cf2b
subhadipClnsn <subhadip.hazra@collinsongroup.com>
2026-03-16 21:41:01 +0530

    use one font family

--- src/pages/injection/[id].tsx
+++ src/pages/injection/[id].tsx
@@ -299,7 +299,6 @@ export default function InjectionGame() {
               padding: 18,
               resize: "vertical",
               fontSize: 15,
-              fontFamily: "'Space Mono','monospace'",
             }}
           />
           <button
